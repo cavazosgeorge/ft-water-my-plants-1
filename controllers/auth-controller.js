cb730c19b1752dc500b36eb6135333f84e655753
George Cavazos <cavazosgeorge@yahoo.com>
2021-11-14 23:08:08 -0500

    Controlers: Added Register endpoint | working

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -2,3 +2,28 @@ const Users = require("../models/users-model");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { checkUsernameExists } = require("../middlewares/auth-middleware");
+
+exports.registerUser = (req, res) => {
+  const { username, password, phoneNumber } = req.body;
+
+  Users.insert({
+    username,
+    password: bcrypt.hashSync(password, 8),
+    phoneNumber,
+  })
+    .then((id) => {
+      res.status(201).json({
+        sucess: true,
+        message: "User registered successfully",
+        data: id,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        success: false,
+        message: err.message,
+        errorMessage: "Error registering user",
+      });
+    });
+};
