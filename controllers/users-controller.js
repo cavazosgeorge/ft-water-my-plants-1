97c3ba44f6979f48497646ef6aaaa3146a853475
George Cavazos <cavazosgeorge@yahoo.com>
2021-11-14 23:10:48 -0500

    Controllers: Created getUsers endpoint | working

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,1 +1,12 @@
 const User = require("../models/users-model");
+
+exports.getUsers = (req, res, next) => {
+  User.find()
+    .then((users) => {
+      res.json({
+        success: true,
+        data: users,
+      });
+    })
+    .catch(next);
+};
