899c7969b1cc3aba9169a003293089fcc978b591
George Cavazos <cavazosgeorge@yahoo.com>
2021-11-15 00:24:06 -0500

    Controllers: Login endpoint created

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -27,3 +27,44 @@ exports.registerUser = (req, res) => {
       });
     });
 };
+
+exports.loginUser =
+  (checkUsernameExists,
+  (req, res, next) => {
+    const { username, password } = req.body;
+
+    Users.findByUsername(username)
+      .then((user) => {
+        let bcryptPass = bcrypt.compareSync(password, user.password);
+
+        if (user && bcryptPass) {
+          const token = generateToken(user);
+          res.status(200).json({
+            success: true,
+            message: "User logged in successfully",
+            token,
+            user,
+          });
+        } else {
+          res.status(401).json({
+            success: false,
+            errorMessage: "User credentials failed",
+          });
+        }
+      })
+      .catch(next);
+  });
+
+// GENERATE TOKEN
+function generateToken(user) {
+  const payload = {
+    username: user.username,
+    id: user.id,
+  };
+
+  const options = {
+    expiresIn: "1d",
+  };
+
+  return jwt.sign(payload, process.env.JWT_SECRET, options);
+}
