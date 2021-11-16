772baa41e9a300459b3a20df62c76826fa96ceab
George Cavazos <cavazosgeorge@yahoo.com>
2021-11-16 05:38:23 -0500

    Controller: getPlantsById endpoint created

--- controllers/plants-controller.js
+++ controllers/plants-controller.js
@@ -10,3 +10,18 @@ exports.getPlants = (req, res, next) => {
     })
     .catch(next);
 };
+
+exports.getPlantsById = (req, res) => {
+  const { id } = req.params;
+  Plants.findById(id)
+    .then((plant) => {
+      if (plant) {
+        res.json(plant);
+      } else {
+        res.status(404).json({ message: "Could not find plant by id given" });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ message: "Failed to get plant" });
+    });
+};
